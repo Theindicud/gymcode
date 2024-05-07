941c2ab8334cb2513b20dd5eb029c63c2747a8d8
RaquelMelow <raquelmelo949@gmail.com>
2024-05-07 18:19:49 +0100

    Check

--- api/middlewares/cors.middleware.js
+++ api/middlewares/cors.middleware.js
@@ -1,8 +1,12 @@
 function cors (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
-    res.setHeader("Access-Control-Allow-Headers", "content-type,authorization");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
 
     next();
 }
 
 module.exports = cors;
+
